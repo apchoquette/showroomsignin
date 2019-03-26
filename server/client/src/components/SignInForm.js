8b4bee5d136da7f91b996e9d57efc19a0ddd2ba8
Andy Choquette <apchoquette@gmail.com>
2019-03-26 10:49:40 -0700

    added logic for new/repeat and pro/homeowner

--- server/client/src/components/SignInForm.js
+++ server/client/src/components/SignInForm.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-import { Form, Button, ButtonGroup} from 'react-bootstrap';
+import { Col, Form, Button, ButtonGroup, Row, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container'
 
 class SignInForm extends Component {
 
+    constructor(){
+        super();
+        this.state = {
+            status: 'Returning', //New or Returning
+            email: '',
+            firstName: '',
+            lastName: '',
+            industry: '',
+            companyName: '',
+            street: '',
+            city: '',
+            state: '',
+            zip: '',
+            classification: '' // homeowner or professional
+        }
+            
+
+    }
+
+
+
     render(){
+
+        const { status, classification } = this.state;
 
         let containerStyle = {
             width: '40%',
@@ -14,30 +37,102 @@ class SignInForm extends Component {
             boxShadow: '0px 2px 4px lightgray',
             padding: '25px'
         }
+
+        console.log(this.state)
+
+        let stateArray = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
+        let industryArray = [ "Interior Design", "Architecture", "Fabrication", "Retail", "Construction", "Wholesale"]
+
+        
+
+     
         return(
             <Container style={containerStyle}>
                 <Form>
-                    <ButtonGroup size="lg">
-                        <Button>First Time</Button>
-                        <Button>Returning</Button>
-                    </ButtonGroup>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Row>
+                        <Col>
+                            <Form.Group controlId="formNewOrRepeat">
+                                <ToggleButtonGroup size="lg" toggle="true" name="status" onClick={(e)=>this.setState({status: e.target.value})}>
+                                    <ToggleButton  value="new">First Time</ToggleButton>
+                                    <ToggleButton  value="returning">Returning</ToggleButton>
+                                </ToggleButtonGroup>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group controlId="formNewOrRepeat">
+                                <ToggleButtonGroup size="lg" toggle="true" name="classification" onClick={(e)=>this.setState({classification: e.target.value})}>
+                                    <ToggleButton value="homeowner">Homeowner</ToggleButton>
+                                    <ToggleButton value="professional">Professional</ToggleButton>
+                                </ToggleButtonGroup>
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+                    
+                    <Form.Group controlId="formBasicEmail" onChange={(e)=>this.setState({email: e.target.value})}>
+                        <Form.Control type="email" placeholder="Email Address" />
                         <Form.Text autocomplete="false" className="text-muted">
                         We'll never share your email with anyone else.
                         </Form.Text>
                     </Form.Group>
-
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                    {status === 'new' && <>
+                    <Form.Group controlId="formName">
+                        <Form.Row>
+                            <Col>
+                                <Form.Control autocomplete="given-name" type="text" placeholder="First Name" onChange={(e)=>this.setState({firstName: e.target.value})}/>
+                            </Col>
+                            <Col>
+                                <Form.Control autocomplete="family-name" type="text" placeholder="Last Name" onChange={(e)=>this.setState({lastName: e.target.value})}/>
+                            </Col>                       
+                        </Form.Row>
                     </Form.Group>
+                    
+                    
+                    {classification === 'professional' && <>
+                    <Form.Group controlId="formIndustry" onChange={(e)=>this.setState({industry: e.target.value})}>
+                        <Form.Control as="select">
+                            <option>Industry</option>
+                            {industryArray.map(industry=>{
+                                return <option>{industry}</option>
+                            })}
+                        </Form.Control>
+
+                    </Form.Group>
+                    <Form.Group controlId="formCompanyName" onChange={(e)=>this.setState({companyName: e.target.value})}>
+                        <Form.Control type="text" placeholder="Company Name" />
+                    </Form.Group>
+                    </>
+                    }
+                    <Form.Group controlId="formAddress" onChange={(e)=>this.setState({address: e.target.value})}>
+                        <Form.Control type="text" placeholder="Street" />
+                    </Form.Group>
+                    
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formGridCity" onChange={(e)=>this.setState({city: e.target.value})}>
+                            <Form.Label>City</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridState" onChange={(e)=>this.setState({state: e.target.value})}>
+                            <Form.Label>State</Form.Label>
+                            <Form.Control as="select">
+                                <option>Choose...</option>
+                                {stateArray.map(state => {
+                                    return <option>{state}</option>
+                                })}
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridZip" onChange={(e)=>this.setState({zip: e.target.value})}>
+                            <Form.Label>Zip</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+                    </Form.Row>
                     <Form.Group controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Check me out" />
                     </Form.Group>
+                    </>}
                     <Button variant="primary" type="submit">
-                        Submit
+                        Sign In
                     </Button>
                 </Form>
             </Container>
