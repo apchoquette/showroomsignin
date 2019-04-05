2e7a93a39ad2e17886e8d6a638ea5199d35d67b7
Andy Choquette <apchoquette@gmail.com>
2019-04-05 08:17:11 -0700

    initial production deployment

--- server/server.js
+++ server/server.js
@@ -10,8 +10,21 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 const PORT = 3100
 
+if (process.env.NODE_ENV === 'production') {
+  //express will serve up production assets
+  app.use(express.static('client/build'))
+  //
+
+  const path = require('path');
+
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 app.listen(PORT, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
+
 
 require('./routes/query')(app);
