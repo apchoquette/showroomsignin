f597abc3fc93c82f2a17bf20dc64f768eeadeba5
Andy Choquette <apchoquette@gmail.com>
2019-04-06 08:43:22 -0700

    updated logo image hosting location to us west

--- server/client/src/components/CompanyLogoLink.js
+++ server/client/src/components/CompanyLogoLink.js
@@ -5,6 +5,6 @@ export default () => {
         margin: '10px'
     }
     return (
-        <img style = {imgStyle} alt="A  K  D  O" src="https://s3.us-east-2.amazonaws.com/akdosignin/logos/logo.png" />
+        <img style = {imgStyle} alt="A  K  D  O" src="https://s3-us-west-2.amazonaws.com/akdomedia/logo.png" />
     )
 }
